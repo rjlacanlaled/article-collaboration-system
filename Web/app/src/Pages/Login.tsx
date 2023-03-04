7bee706354d1149d4adaea510f2394c4c040c6a7
bryansaguit <fakerbryan@yahoo.com>
2023-03-04 22:54:40 +0800

    Add login page & dependencies

---
+++ Web/app/src/Pages/Login.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import { ReactComponent as GoogleIcon} from "../Assets/Images/google-logo.svg"
+import { ReactComponent as FacebookIcon} from "../Assets/Images/facebook-logo.svg"
+import { ReactComponent as LinkedinIcon} from "../Assets/Images/linkedin-logo.svg"
+
+function Login() {
+  return (
+    <>
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+        <div className="bg-white p-8 w-96  rounded-lg shadow-lg flex justify-center items-center flex-col">
+            <h2 className="mb-6 font-bold text-3xl text-gray-800">Welcome Back</h2>
+            <div className="mb-4">
+                <input className="shadow appearance-none border rounded-sm w-80 py-4 px-3 text-gray-800 placeholder-gray-500 leading-tight focus:outline-none focus:shadow-outline focus:border-sky-500" type="text" placeholder="Email address"/>
+            </div>
+            <div className="mb-4">
+                <input className="shadow appearance-none border rounded-sm w-80 py-4 px-3 text-gray-800 placeholder-gray-500 leading-tight focus:outline-none focus:shadow-outline focus:border-sky-500" type="password" placeholder="Password"/>
+            </div>
+            <button className="bg-blue-500 hover:bg-blue-600 transition duration-300 text-white w-80 py-4 px-4 rounded-sm">
+                Continue
+            </button>
+            <h2 className="text-xs mt-4 text-gray-800">Don't Have an account?<a href='/' className='ml-1 text-blue-500'>Sign Up</a></h2>
+            <div className="mt-5 grid grid-cols-3 items-center text-gray-400"> 
+                <hr className="border-gray-400 w-24"/>
+                    <p className='text-sm text-center text-gray-800'>OR</p>
+                <hr className="border-gray-400 w-24"/>
+            </div>
+            <button className="bg-white text-gray-800 shadow appearance-none border py-4 px-4 w-80 rounded-sm mt-5 text-sm hover:bg-gray-100 transition duration-300 flex justify-start items-center"><GoogleIcon className="w-6 h-full mr-3"/> Continue With Google</button>
+            <button className="bg-white text-gray-800 shadow appearance-none border py-4 px-4 w-80 rounded-sm mt-3 text-sm hover:bg-gray-100 transition duration-300 flex justify-start items-center"><FacebookIcon className="w-6 h-full mr-3"/>Continue With Facebook</button>
+            <button className="bg-white text-gray-800 shadow appearance-none border py-4 px-4 w-80 rounded-sm mt-3 text-sm hover:bg-gray-100 transition duration-300 flex justify-start items-center"><LinkedinIcon className="w-6 h-full mr-3"/>Continue With LinkedIn</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Login
