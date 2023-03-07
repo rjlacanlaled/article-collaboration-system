40deefce0a584d1e1c9f6c78ae4074ca0ca23a21
bryansaguit <fakerbryan@yahoo.com>
2023-03-07 23:23:56 +0800

    add success signup page

---
+++ Web/app/src/Components/SignupSuccess.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SuccessIcon from "../Assets/Images/signup-success.svg"
+
+function SignupSuccess() {
+  return (
+    <> 
+      <div className="bg-white w-4/12 p-8 h-5/6 rounded-lg shadow-lg flex items-center justify-center flex-col">
+        <img src={SuccessIcon} alt="success-logo" className="w-64 h-64 my-6" />
+        <h1 className="text-blue-500 text-3xl font-bold leading-relaxed text-center my-4">SUCCESS</h1>
+        <p className="text-lg leading-relaxed text-center w-3/4 ">Thank you for signing up! Your account is currently being reviewed and you will receive an email confirmation once it has been approved.</p>
+        <button className="bg-blue-500 hover:bg-blue-600 transition duration-300 text-white w-80 py-4 px-4 rounded-lg my-10">Continue</button>
+      </div>
+    </>
+  )
+}
+
+export default SignupSuccess
---
+++ Web/app/src/Pages/SignupSuccessPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupSuccess from "../Components/SignupSuccess";
+
+function SignupSuccessPage() {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center flex-col">
+        <SignupSuccess />
+    </div>
+  )
+}
+
+export default SignupSuccessPage
