fd2b15800e41324f7be8acccefb550c900d2f84d
bryansaguit <fakerbryan@yahoo.com>
2023-03-08 01:41:38 +0800

    Add Reset Password Page

---
+++ Web/app/src/Components/ResetPassword.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ForgotIcon from "../Assets/Images/forgot-password.svg"
+
+function ResetPassword() {
+  return (
+    <div className="bg-white p-8 w-96  rounded-lg shadow-lg flex justify-center items-center flex-col">
+        <img src={ForgotIcon} alt="Forgot-Icon" className="w-32 h-full my-11" />
+        <h2 className="mb-6 font-bold text-3xl text-gray-800">Reset your password</h2>
+        <p className="text-sm text-center w-11/12">Passwords are like underwear: you don’t let people see it, you should change it very often, and you shouldn’t share it with strangers.</p>
+        <div className="mb-4 mt-6">
+            <input className="shadow appearance-none border rounded-sm w-80 py-4 px-3 text-gray-800 placeholder-gray-500 leading-tight focus:outline-none focus:shadow-outline focus:border-sky-500" type="text" placeholder="Email address"/>
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-600 transition duration-300 text-white w-80 py-4 px-4 mb-7 rounded-sm">
+            Continue
+        </button>
+    </div>
+  )
+}
+
+export default ResetPassword
---
+++ Web/app/src/Pages/ResetPasswordPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ResetPassword from '../Components/ResetPassword'
+
+function ResetPasswordPage() {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+        <ResetPassword />
+    </div>
+  )
+}
+
+export default ResetPasswordPage
